93d17ad117f165a69aac016a000cc321aee18d9a
Nurazizah323 <nurazizahn323@gmail.com>
2021-10-21 22:51:57 +0700

    commit on master

--- application/views/Review/v_input.php
+++ application/views/Review/v_input.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Input</title>
 </head>
 
 <style>
-  fieldset
+  fieldset 
   {
     width: 40%;
     margin: 20px auto;
@@ -21,4 +21,5 @@
     <legend>Input Transaksi</legend>
   </fieldset>
 </body>
+
 </html>
