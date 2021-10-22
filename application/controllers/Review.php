468d36108841dbe248ee290a11b8c1d781320a28
Nurazizah323 <nurazizahn323@gmail.com>
2021-10-22 23:26:44 +0700

    commit on master

--- application/controllers/Review.php
+++ application/controllers/Review.php
@@ -5,6 +5,16 @@ class Review extends CI_Controller
 {
   public function index()
   {
-    $this->load->view('review/v_input');
+    $this->form_validation->set_rules('nama','Nama Pembeli','required',
+    ['required' ⇒ 'Nama Pembeli harus di isi']);
+    $this->form_validation->set_rules('nhp','Nomer HP','required',
+    ['required' ⇒ 'Nomer HP harus di isi']);
+    if ($this->form_validation->run() = false) {
+      $data['merk'] = ['Nike','Adidas','Kickers','Eiger','Bucherri'];
+      $this->load->view('review/v_input',$data);
+    } else {
+      var_dump($_POST);
+      die;
+    }
   }
 }
